44c506d3be6891efb82d6a986522529bbec0c15f
madhavannov <madhavanperumal88@gmail.com>
2025-12-15 21:10:11 +0530

    Add requirements.txt and update settings.py for production deployment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--cw)b-%ci&v2b^*ih2ec&s--%3a#u#szfnzoux0y09&jfs#d!6'
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--cw)b-%ci&v2b^*ih2ec&s--%3a#u#szfnzoux0y09&jfs#d!6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else []
 
 
 # Application definition
